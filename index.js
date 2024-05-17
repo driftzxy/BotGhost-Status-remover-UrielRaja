cd77d0ad48d384cdd3fb68d6fea40993e3aad022
Urichuche <urielrape6@gmail.com>
2024-05-17 18:07:49 +0200

    index.js
    17/05

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Viendo /help","Gracias a UrielRaja por crearme <3"];
 
 
 let currentIndex = 0;
